01bb2f05f0278ae1e19a2365892cb3eded3ef60f
Martin Oehler <oehler@sim.tu-darmstadt.de>
2021-11-26 14:34:45 +0100

    Disabled silent update so errors can be seen

--- scripts/hooks/update.sh
+++ scripts/hooks/update.sh
@@ -14,10 +14,10 @@ echo_info ">>> Pulling system package updates"
 if [[ $_NO_SUDO == 1 ]]; then
     echo "Skipped because --no-sudo option was specified."
 else
-    sudo apt-get update -qq
+    sudo apt update
     AVAILABLE_SYSTEM_PACKAGE_UPDATES=$(apt-get -qq -s upgrade | grep Inst | cut -d ' ' -f 2 | grep "^[[:blank:]]*${ROSWSS_PREFIX}-\|^[[:blank:]]*ros-")
     if [ ! -z "${AVAILABLE_SYSTEM_PACKAGE_UPDATES[@]}" ]; then
-        sudo apt install -qq --only-upgrade ${AVAILABLE_SYSTEM_PACKAGE_UPDATES[@]}
+        sudo apt install --only-upgrade ${AVAILABLE_SYSTEM_PACKAGE_UPDATES[@]}
     else
         echo "Already up to date."
     fi
