8c5b6104103f939db8f85749e6ddb80fe8901d3e
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-01-02 18:16:02 +0100

    Changed indentation.

--- scripts/hooks/update/10.system-packages.sh
+++ scripts/hooks/update/10.system-packages.sh
@@ -2,15 +2,11 @@
 . ${TUDA_WSS_BASE_SCRIPTS}/helpers/output.sh
 
 _NO_SUDO=0
-for arg in "$@"; do
-    # Exclude arguments passed with -*, e.g., --no-sudo
-    if [ "$arg" = "--no-sudo" ]; then
-        _NO_SUDO=1
-    fi
-done
-
 _DEFAULT_YES=""
 for arg in "$@"; do
+  if [ "$arg" = "--no-sudo" ]; then
+    _NO_SUDO=1
+  fi
   if [ "$arg" = "--default-yes" ] || [ "$arg" = "-y" ]; then
     _DEFAULT_YES="-y"
   fi
@@ -20,13 +16,13 @@ done
 # pull system update
 _echo_header "Pulling ROS system package updates"
 if [ $_NO_SUDO -eq 1 ]; then
-    echo "Skipped because --no-sudo option was specified."
+  echo "Skipped because --no-sudo option was specified."
 else
-    sudo apt-get update
-    AVAILABLE_SYSTEM_PACKAGE_UPDATES=$(apt-get -qq -s upgrade | grep Inst | cut -d ' ' -f 2 | grep "^[[:blank:]]*ros-")
-    if [ -n "${AVAILABLE_SYSTEM_PACKAGE_UPDATES}" ]; then
-        sudo apt install ${_DEFAULT_YES} --only-upgrade ${AVAILABLE_SYSTEM_PACKAGE_UPDATES}
-    else
-        echo "Already up to date."
-    fi
+  sudo apt-get update
+  AVAILABLE_SYSTEM_PACKAGE_UPDATES=$(apt-get -qq -s upgrade | grep Inst | cut -d ' ' -f 2 | grep "^[[:blank:]]*ros-")
+  if [ -n "${AVAILABLE_SYSTEM_PACKAGE_UPDATES}" ]; then
+    sudo apt install ${_DEFAULT_YES} --only-upgrade ${AVAILABLE_SYSTEM_PACKAGE_UPDATES}
+  else
+    echo "Already up to date."
+  fi
 fi
