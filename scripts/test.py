38f5344bfe83d6a253501c2d2492e2af3c5fc7f1
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-01-31 16:33:22 +0100

    Added support to use asan-gcc and tsan to check for memory / data race issues while running tests.

--- scripts/test.py
+++ scripts/test.py
@@ -11,9 +11,38 @@ import sys
 if __name__ == "__main__":
     workspace_root = get_workspace_root()
     parser = argparse.ArgumentParser()
-    packages_arg = parser.add_argument('packages', nargs='*', help='If specified only these packages are built.')
+    packages_arg = parser.add_argument(
+        "packages", nargs="*", help="If specified only these packages are built."
+    )
     packages_arg.completer = PackageChoicesCompleter(workspace_root)
-    parser.add_argument('--this', default=False, action='store_true', help='Test the packages in the current directory.')
+    parser.add_argument(
+        "--this",
+        default=False,
+        action="store_true",
+        help="Test the packages in the current directory.",
+    )
+    parser.add_argument(
+        "--memory-check",
+        default=False,
+        action="store_true",
+        help="Check for memory issues during test execution using asan.",
+    )
+    parser.add_argument(
+        "--thread-check",
+        default=False,
+        action="store_true",
+        help="Check for data race issues during test execution using tsan.",
+    )
+    parser.add_argument(
+        "--clean", default=False, action="store_true", help="Clean before building."
+    )
+    parser.add_argument(
+        "--yes",
+        "-y",
+        default=False,
+        action="store_true",
+        help="Automatically answer yes to all questions.",
+    )
 
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
@@ -33,30 +62,84 @@ if __name__ == "__main__":
             print_error("No package found in the current directory!")
             exit(1)
 
+    build_folder = "build"
+    install_folder = "install"
+    mixin = []
+    if args.memory_check and args.thread_check:
+        print_error("Memory and thread check cannot be enabled at the same time!")
+        exit(1)
+    elif args.memory_check:
+        # Apply workaround for asan issue in rcutils
+        asan_options = os.environ.get("ASAN_OPTIONS", "")
+        if len(asan_options) > 0:
+            asan_options += ":"
+        os.environ["ASAN_OPTIONS"] = (
+            asan_options + "new_delete_type_mismatch=0:verify_asan_link_order=0"
+        )
+        mixin.append("asan-gcc")
+        build_folder = "build/asan"
+        install_folder = "install/asan"
+    elif args.thread_check:
+        mixin.append("tsan")
+        build_folder = "build/tsan"
+        install_folder = "install/tsan"
+
     os.chdir(workspace_root)
-    print_info('>>> Building packages')
+    if args.clean and not clean_packages(
+        workspace_root,
+        packages,
+        force=args.yes,
+        build_base=build_folder,
+        install_base=install_folder,
+    ):
+        sys.exit(1)
+    print_info(">>> Building packages")
+    returncode = build_packages(
+        workspace_root,
+        packages=packages if len(packages) > 0 else None,
+        mixin=mixin,
+        build_tests=True,
+        build_base=build_folder,
+        install_base=install_folder,
+    )
+    if returncode != 0:
+        print_error(">>> Failed to build packages")
+        exit(returncode)
+
+    print_info(">>> Running tests")
+    colcon_test_args = []
+    if build_folder is not None:
+        colcon_test_args.extend(["--build-base", build_folder])
+    if install_folder is not None:
+        colcon_test_args.extend(["--install-base", install_folder])
     if len(packages) > 0:
-        returncode = build_packages(workspace_root, packages, build_tests=True)
-        if returncode != 0:
-            print_error('>>> Failed to build packages')
-            exit(returncode)
-        print_info('>>> Running tests')
-        command = subprocess.run('colcon test --packages-select ' + ' '.join(packages),
-                                 stdout=sys.stdout, stderr=sys.stderr, shell=True)
-        returncode = command.returncode
+        colcon_test_args.extend(["--packages-select"] + packages)
+
+    command = subprocess.run(
+        f". {install_folder}/setup.sh && colcon test {' '.join(colcon_test_args)}",
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        shell=True,
+    )
+    returncode = command.returncode
+
+    build_folder = build_folder or "build"
+    if len(packages) > 0:
         for package in packages:
-            print_info(f'>>> {package}')
-            command = subprocess.run(f'colcon test-result --verbose --test-result-base build/{package}',
-                                     stdout=sys.stdout, stderr=sys.stderr, shell=True)
+            print_info(f">>> {package}")
+            command = subprocess.run(
+                f"colcon test-result --verbose --test-result-base {build_folder}/{package}",
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+                shell=True,
+            )
             returncode |= command.returncode
     else:
-        returncode = build_packages(workspace_root, build_tests=True)
-        if returncode != 0:
-            print_error('>>> Failed to build packages')
-            exit(returncode)
-        print_info('>>> Running tests')
-        command = subprocess.run('colcon test', stdout=sys.stdout, stderr=sys.stderr, shell=True)
-        returncode = command.returncode
-        command = subprocess.run('colcon test-result --verbose', stdout=sys.stdout, stderr=sys.stderr, shell=True)
+        command = subprocess.run(
+            "colcon test-result --verbose --test-result-base build",
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            shell=True,
+        )
         returncode |= command.returncode
     sys.exit(returncode)
--- tuda_workspace_scripts/build.py
+++ tuda_workspace_scripts/build.py
@@ -26,14 +26,14 @@ def build_packages(
     build_tests: bool = False,
     mixin: list[str] = None,
     verbose: bool = False,
-    build_base: str | None = None,
-    install_base: str | None = None,
+    build_base: str = "build",
+    install_base: str = "install",
 ) -> int:
     os.chdir(workspace_root)
     arguments = []
-    if build_base is not None:
+    if build_base != "build":
         arguments += ["--build-base", build_base]
-    if install_base is not None:
+    if install_base != "install":
         arguments += ["--install-base", install_base]
     if colcon_override_check is not None and any(packages):
         arguments += ["--allow-overriding"] + packages
@@ -88,17 +88,23 @@ def clean_logs(workspace_root, packages=None, force=False):
     os.chdir(original_path)
 
 
-def clean_packages(workspace_root, packages, force=False) -> bool:
+def clean_packages(
+    workspace_root,
+    packages,
+    force=False,
+    build_base: str = "build",
+    install_base: str = "install",
+) -> bool:
     original_path = os.getcwd()
     os.chdir(workspace_root)
     try:
         print("I will delete:")
         if any(packages):
-            print(f'  {os.getcwd()}/build/{{{",".join(packages)}}}')
-            print(f'  {os.getcwd()}/install/{{{",".join(packages)}}}')
+            print(f'  {os.getcwd()}/{build_base}/{{{",".join(packages)}}}')
+            print(f'  {os.getcwd()}/{install_base}/{{{",".join(packages)}}}')
         else:
-            print(f"  {os.getcwd()}/build")
-            print(f"  {os.getcwd()}/install")
+            print(f"  {os.getcwd()}/{build_base}")
+            print(f"  {os.getcwd()}/{install_base}")
             print(f"  {os.getcwd()}/log")
 
         if not force and not confirm("Continue?"):
@@ -107,14 +113,14 @@ def clean_packages(workspace_root, packages, force=False) -> bool:
 
         if any(packages):
             for package in packages:
-                shutil.rmtree(f"build/{package}", ignore_errors=True)
-                shutil.rmtree(f"install/{package}", ignore_errors=True)
+                shutil.rmtree(f"{build_base}/{package}", ignore_errors=True)
+                shutil.rmtree(f"{install_base}/{package}", ignore_errors=True)
             ament_prefix_path = get_ament_prefix_path_without_packages(packages)
             cmake_prefix_path = get_cmake_prefix_path_without_packages(packages)
             print_info(f">>> {len(packages)} package cleaned.")
         else:
-            shutil.rmtree("build", ignore_errors=True)
-            shutil.rmtree("install", ignore_errors=True)
+            shutil.rmtree(build_base, ignore_errors=True)
+            shutil.rmtree(install_base, ignore_errors=True)
             shutil.rmtree("log", ignore_errors=True)
             ament_prefix_path = get_ament_prefix_path_without_workspace(workspace_root)
             cmake_prefix_path = get_cmake_prefix_path_without_workspace(workspace_root)
