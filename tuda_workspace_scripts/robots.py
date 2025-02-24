870100f8f6df80be87f2fa708822af1df3f93c08
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-02-24 10:39:44 +0100

    Fixed zenoh protocol order.

--- tuda_workspace_scripts/robots.py
+++ tuda_workspace_scripts/robots.py
@@ -80,7 +80,7 @@ class ZenohRouter:
         self.protocol = protocol
 
     def get_zenoh_router_address(self):
-        return f"{self.address}:{self.port}/{self.protocol}"
+        return f"{self.protocol}/{self.address}:{self.port}"
 
 
 class Robot:
