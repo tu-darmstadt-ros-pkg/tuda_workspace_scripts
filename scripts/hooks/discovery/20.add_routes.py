4e8eecf4d409a88e1d2b64b220f2c5d13fadd163
Frederik Bark <bark@sim.tu-darmstadt.de>
2025-11-21 10:44:37 +0100

    linting

--- scripts/hooks/discovery/20.add_routes.py
+++ scripts/hooks/discovery/20.add_routes.py
@@ -32,30 +32,29 @@ def on_discovery_updated(robots, selected_robots, **_):
         parts = pc_ip.split(".")[:-1]
         net_ip = ".".join(parts + ["0"])
         host_ip = ".".join(parts[:2] + ["0", parts[2]])
-        
-        existing_routes = ip.get_routes(dst=net_ip + '/' + str(netmask))
+
+        existing_routes = ip.get_routes(dst=net_ip + "/" + str(netmask))
 
         valid_route = False
         if existing_routes:
             gateway = next(
                 (
-                    value for key, value in existing_routes[0]["attrs"]
+                    value
+                    for key, value in existing_routes[0]["attrs"]
                     if key == "RTA_GATEWAY"
                 ),
                 None,
             )
             if gateway == host_ip:
                 valid_route = True
-            
+
         if not valid_route:
-            print(
-                f"Adding route to {net_ip}/{netmask} via host pc"
-            )
+            print(f"Adding route to {net_ip}/{netmask} via host pc")
             command = (
                 f"ip.route('add', dst='{net_ip}/{netmask}', gateway='{host_ip}')\n"
             )
             routes_to_add += command
-    
+
     if routes_to_add != "":
         ip_route_code = f"""from pyroute2 import IPRoute
 ip = IPRoute()
@@ -64,7 +63,4 @@ ip = IPRoute()
         print(
             "Privilege escalation required to add routes, running commands with sudo..."
         )
-        subprocess.run(
-            ["sudo", "python3", "-c", ip_route_code],
-            check=True
-        )
+        subprocess.run(["sudo", "python3", "-c", ip_route_code], check=True)
