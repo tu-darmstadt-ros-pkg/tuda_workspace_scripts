ee26b6f3df6f5b2826a1617f1113ff98da79afa8
Aljoscha Schmidt <aljoscha.schmidt@tu-darmstadt.de>
2025-12-19 17:30:48 +0100

    added --this option to clean cmd

--- scripts/_clean.py
+++ scripts/_clean.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 from tuda_workspace_scripts.build import clean_logs, clean_packages
-from tuda_workspace_scripts.print import print_workspace_error
-from tuda_workspace_scripts.workspace import get_workspace_root, PackageChoicesCompleter
+from tuda_workspace_scripts.print import print_error, print_workspace_error
+from tuda_workspace_scripts.workspace import (
+    get_workspace_root,
+    PackageChoicesCompleter,
+    find_package_containing,
+    find_packages_in_directory,
+)
 from helpers.remove_packages_from_env import *
 import argcomplete
 import argparse
+import os
 
 
 if __name__ == "__main__":
@@ -16,6 +22,12 @@ if __name__ == "__main__":
         "packages", nargs="*", help="If specified only these packages are cleaned."
     )
     packages_arg.completer = PackageChoicesCompleter(workspace_root)
+    parser.add_argument(
+        "--this",
+        default=False,
+        action="store_true",
+        help="Clean the package(s) in the current directory.",
+    )
     parser.add_argument("--force", default=False, action="store_true")
     parser.add_argument(
         "--logs",
@@ -31,7 +43,18 @@ if __name__ == "__main__":
         print_workspace_error()
         exit(1)
 
+    packages = args.packages or []
+    if args.this:
+        packages = find_packages_in_directory(os.getcwd())
+        if len(packages) == 0:
+            # No packages in the current folder but maybe the current folder is in a package
+            package = find_package_containing(os.getcwd())
+            packages = [package] if package else []
+        if len(packages) == 0:
+            print_error("No package found in the current directory!")
+            exit(1)
+
     if args.logs:
-        exit(clean_logs(workspace_root, args.packages or [], force=args.force))
+        exit(clean_logs(workspace_root, packages, force=args.force))
     else:
-        exit(clean_packages(workspace_root, args.packages or [], force=args.force))
+        exit(clean_packages(workspace_root, packages, force=args.force))
