3e7cffb7a41435c3be788c8e293e2a26e3e3d5e4
Jonathan <jonathan.lichtenfeld@gmail.com>
2026-03-02 18:53:19 +0100

    Setup discovery for zenoh bridge

--- tuda_workspace_scripts/discovery.py
+++ tuda_workspace_scripts/discovery.py
@@ -12,11 +12,15 @@ ws_root = get_workspace_root()
 if not ws_root:
     raise RuntimeError("Workspace root not found")
 RMW: str | None = os.getenv("RMW_IMPLEMENTATION", None)
+ZENOH_BRIDGE_CONFIG_URI: str | None = os.getenv("ZENOH_BRIDGE_CONFIG_URI", None)
 CYCLONEDDS_URI: str | None = os.getenv("CYCLONEDDS_URI", None)
 ZENOH_ROUTER_CONFIG_PATH: str | None = os.getenv("ZENOH_ROUTER_CONFIG_URI", None)
 XML_MARKER = "<!-- This file is managed by tuda_workspace_scripts. Changes may be overwritten. -->"
 YAML_MARKER = (
     "# This file is managed by tuda_workspace_scripts. Changes may be overwritten."
+)
+JSON5_MARKER = (
+    "// This file is managed by tuda_workspace_scripts. Changes may be overwritten."
 )
 
 
@@ -28,7 +32,7 @@ def create_discovery_config(selected_robots: list[str], custom_addresses: list[s
             selected_robots, available_robots, custom_addresses
         )
     elif RMW == "rmw_cyclonedds_cpp":
-        create_cyclonedds_router_config_xml(
+        create_zenoh_bridge_config_json5(
             selected_robots, available_robots, custom_addresses
         )
     elif RMW:
@@ -37,71 +41,63 @@ def create_discovery_config(selected_robots: list[str], custom_addresses: list[s
         raise RuntimeError("RMW_IMPLEMENTATION is not set.")
 
 
-def create_cyclonedds_router_config_xml(
+def create_zenoh_bridge_config_json5(
     selected_robots: list[str],
     available_robots: dict[str, Robot],
     custom_addresses: list[str],
 ):
-    peers = []
-
-    # Always set localhost, even if the user did not specify it
-    peers.append("127.0.0.1")
+    routers = []
 
     for name in selected_robots:
         if name == "off":
             break
         elif name == "all":
             for _, robot_data in available_robots.items():
-                if not robot_data.cyclonedds_address:
-                    print_warn(f"No Cyclone DDS address found for {robot_data.name}")
-                    continue
-                peers.append(robot_data.cyclonedds_address)
+                routers.extend(robot_data.zenoh_routers)
             break
         elif name in available_robots:
-            if not available_robots[name].cyclonedds_address:
-                print_warn(
-                    f"No Cyclone DDS address found for {available_robots[name].name}"
-                )
-                continue
-            peers.append(available_robots[name].cyclonedds_address)
+            routers.extend(available_robots[name].zenoh_routers)
         else:
             print_warn(
                 f"Couldn't find an entry for {name} in robot configs. Please check if your selected robot is available."
             )
 
-    ipv4_regex = r"^(?:\d{1,3}\.){3}\d{1,3}$"
-    hostname_regex = r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z0-9-]{1,63})*$"
     for address in custom_addresses:
-        match = re.match(ipv4_regex, address) or re.match(hostname_regex, address)
+        match = re.match(r"([^:/]+)(:\d+)?(/.*)?$", address)
         if not match:
             print_warn(
-                f"Invalid address '{address}'! Please specify an IP address or hostname."
+                f"Invalid address '{address}'! Please use the format 'IP_OR_HOSTNAME[:PORT][/PROTOCOL]'."
             )
             continue
-        peers.append(address)
+        name = match.group(1)
+        tmp = match.group(2)
+        port = int(tmp[1:]) if tmp and tmp[0] == ":" else 7448
+        tmp = match.group(3)
+        protocol = tmp[1:] if tmp and tmp[0] == "/" else "tcp"
+        routers.append(ZenohRouter(name, port, protocol))
 
-    config = _create_cyclonedds_config_xml(peers)
-    print("Connecting to peers: ")
-    for peer in peers:
-        print(" -", peer)
+    endpoints = [router.get_zenoh_router_address() for router in routers]
+    config = _create_zenoh_bridge_config_json5(endpoints)
+    print("Zenoh bridge connecting to endpoints:")
+    for endpoint in endpoints:
+        print(" -", endpoint)
 
-    if os.path.isfile(CYCLONEDDS_URI):
+    if os.path.isfile(ZENOH_BRIDGE_CONFIG_URI):
         # Backup existing files if not ours
-        with open(CYCLONEDDS_URI, "r") as file:
-            if file.readline().strip() != XML_MARKER:
+        with open(ZENOH_BRIDGE_CONFIG_URI, "r") as file:
+            if file.readline().strip() != JSON5_MARKER:
                 i = 0
-                while os.path.isfile(CYCLONEDDS_URI + f".backup{i}"):
+                while os.path.isfile(ZENOH_BRIDGE_CONFIG_URI + f".backup{i}"):
                     i += 1
                 print_warn(
-                    f"Existing cyclonedds config found at {CYCLONEDDS_URI}. Backing up as {CYCLONEDDS_URI}.backup{i}."
+                    f"Existing Zenoh bridge config found at {ZENOH_BRIDGE_CONFIG_URI}. Backing up as {ZENOH_BRIDGE_CONFIG_URI}.backup{i}."
                 )
-                os.rename(CYCLONEDDS_URI, f"{CYCLONEDDS_URI}.backup{i}")
+                os.rename(ZENOH_BRIDGE_CONFIG_URI, f"{ZENOH_BRIDGE_CONFIG_URI}.backup{i}")
 
-    with open(CYCLONEDDS_URI, "w", encoding="utf-8") as file:
-        file.write(f"{XML_MARKER}\n")
+    with open(ZENOH_BRIDGE_CONFIG_URI, "w", encoding="utf-8") as file:
+        file.write(f"{JSON5_MARKER}\n")
         file.write(config)
-    print_info(f"Cyclone DDS config updated.")
-
+    print_info(f"Zenoh bridge config updated.")
 
 def create_zenoh_router_config_yaml(
     selected_robots: list[str],
@@ -164,16 +160,17 @@ def create_zenoh_router_config_yaml(
     print_info(f"Zenoh router config updated.")
 
 
-def _create_cyclonedds_config_xml(peers: list[str]) -> str:
+
+def _create_zenoh_bridge_config_json5(endpoints: list[str]) -> str:
     # Get the template directory path from the share directory
     template_dir = os.path.join(
         get_package_share_directory("tuda_workspace_scripts"), "templates"
     )
     env = Environment(loader=FileSystemLoader(template_dir))
-    template = env.get_template("cyclonedds_config.xml.j2")
+    template = env.get_template("zenoh-bridge-config.json5")
 
-    # Render the template with the peers list
-    return template.render(peers=peers)
+    # Render the template with the endpoints list
+    return template.render(endpoints=endpoints)
 
 
 def print_discovery_config():
@@ -181,20 +178,20 @@ def print_discovery_config():
     if RMW == "rmw_zenoh_cpp":
         print_zenoh_discovery_config()
     elif RMW == "rmw_cyclonedds_cpp":
-        print_cyclonedds_discovery_config()
+        print_zenoh_bridge_discovery_config()
     elif RMW:
         raise NotImplementedError(f"Discovery is not implemented for RMW {RMW}")
     else:
         raise RuntimeError("RMW_IMPLEMENTATION is not set.")
 
 
-def print_cyclonedds_discovery_config():
-    if os.path.exists(CYCLONEDDS_URI):
-        print_info(f"Configuration file: {CYCLONEDDS_URI}")
-        with open(CYCLONEDDS_URI, "r") as file:
+def print_zenoh_bridge_discovery_config():
+    if os.path.exists(ZENOH_BRIDGE_CONFIG_URI):
+        print_info(f"Configuration file: {ZENOH_BRIDGE_CONFIG_URI}")
+        with open(ZENOH_BRIDGE_CONFIG_URI, "r") as file:
             print(file.read())
     else:
-        print_warn(f"Configuration file not found: {CYCLONEDDS_URI}")
+        print_warn(f"Configuration file not found: {ZENOH_BRIDGE_CONFIG_URI}")
 
 
 def print_zenoh_discovery_config():
