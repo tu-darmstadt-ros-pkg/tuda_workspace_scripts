e933217cedaf2d5f30ff26d71010f6c583943089
Marek Daniv <marekdaniv@googlemail.com>
2026-07-21 10:49:26 +0200

    Implement fallback if ros2 daemon stop request times out. Ros2 daemon process will be killed directly

--- scripts/hooks/wtf/90.ros2_daemon_restart.py
+++ scripts/hooks/wtf/90.ros2_daemon_restart.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3
+import psutil
+
 from tuda_workspace_scripts.print import *
 from ros2cli.node.daemon import is_daemon_running, spawn_daemon, shutdown_daemon
 import os
+
+
+def find_ros2_daemon() -> int:
+    print_info("ROS2 daemon not responding to shutdown request. Killing it.")
+    for proc in psutil.process_iter(["pid", "name", "cmdline"]):
+        try:
+            cmdline = proc.info["cmdline"]
+            if cmdline and "ros2-daemon" in cmdline:
+                return proc.info["pid"]
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+
+def kill_ros2_daemon() -> int:
+    ros2_daemon_pid = find_ros2_daemon()
+    if ros2_daemon_pid:
+        try:
+            proc = psutil.Process(ros2_daemon_pid)
+            proc.kill()
+            # Make sure the process has terminated and xmlrpc server port is released
+            proc.wait(timeout=1)
+            print_info(f"Killed ROS2 daemon with PID {ros2_daemon_pid}.")
+            return 1
+        except (psutil.NoSuchProcess, psutil.AccessDenied) as e:
+            print_error(f"Failed to kill ROS2 daemon with PID {ros2_daemon_pid}: {e}")
+            return 0
+    else:
+        print_info("No ROS2 daemon process found to kill.")
+        return 1
 
 
 def fix() -> int:
@@ -10,9 +41,10 @@ def fix() -> int:
         try:
             if is_daemon_running(args=[]):
                 print_info("ROS2 daemon is running. Restarting it just to be safe.")
-                if not shutdown_daemon(args=[], timeout=10):
-                    print_error("Failed to shutdown ROS2 daemon")
-                    return 0
+                if not shutdown_daemon(args=[], timeout=1):
+                    if not kill_ros2_daemon():
+                        print_error("Failed to shutdown ROS2 daemon")
+                        return 0
                 if not spawn_daemon(args=[], timeout=10):
                     print_error("Failed to restart ROS2 daemon after stopping it")
                     return 0
